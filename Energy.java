5d4befe3396d0d5d12ba6012dd9061b24a8060b1
jpmaligne <jpmaligne@gmail.com>
2015-12-31 15:19:52 +0100

    Create Energy.java

---
+++ Energy.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * this class correspond with each Energy Type that are implemented in the game : Grass; Fire; Water ....
+ *
+ */
+public class Energy {
+	private String Type;
+	
+	public Energy(String aType){
+		Type = aType;
+	}
+	
+	public String getType(){
+		return Type;
+	}
+}
